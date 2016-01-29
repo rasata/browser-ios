4a9e591971609408addce06387fcd7263e7425d9
Garvan Keeley <garvankeeley@gmail.com>
2016-01-29 17:24:08 -0500

    Private mode sets all cookies to Session-only cookies, these are not stored on disk. TODO add test cases

--- brave/src/webview/PrivateBrowsing.swift
+++ brave/src/webview/PrivateBrowsing.swift
@@ -10,15 +10,18 @@ class PrivateBrowsing {
     var nonprivateCookies = [NSHTTPCookie: Bool]()
 
     func enter() {
+        isOn = true
+
         NSURLCache.sharedURLCache().memoryCapacity = 0;
         NSURLCache.sharedURLCache().diskCapacity = 0;
 
         let storage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
         if let cookies = storage.cookies {
-        for cookie in cookies {
-                    nonprivateCookies[cookie] = true
-                }
+            for cookie in cookies {
+                nonprivateCookies[cookie] = true
+            }
         }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "cookiesChanged:", name: NSHTTPCookieManagerCookiesChangedNotification, object: nil)
     }
 
     func exit() {
@@ -29,16 +32,45 @@ class PrivateBrowsing {
         isOn = false
 
         BraveApp.setupCacheDefaults()
+        NSNotificationCenter.defaultCenter().removeObserver(self)
 
         let storage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
         if let cookies = storage.cookies {
             for cookie in cookies {
                 if nonprivateCookies[cookie] == nil {
-                    storage.deleteCookie(cookie)
+                   storage.deleteCookie(cookie)
                 }
             }
         }
 
         NSUserDefaults.standardUserDefaults().synchronize()
+        nonprivateCookies = [NSHTTPCookie: Bool]()
+    }
+
+    @objc func cookiesChanged(info: NSNotification) {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        let storage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        var newCookies = [NSHTTPCookie]()
+        if let cookies = storage.cookies {
+            for cookie in cookies {
+                if let readOnlyProps = cookie.properties {
+                    var props = readOnlyProps as [String: AnyObject]
+                    let discard = props[NSHTTPCookieDiscard] as? String
+                    if discard == nil || discard! != "TRUE" {
+                        props.removeValueForKey(NSHTTPCookieExpires)
+                        props[NSHTTPCookieDiscard] = "TRUE"
+                        storage.deleteCookie(cookie)
+                        if let newCookie = NSHTTPCookie(properties: props) {
+                            newCookies.append(newCookie)
+                        }
+                    }
+                }
+            }
+        }
+        for c in newCookies {
+            storage.setCookie(c)
+        }
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "cookiesChanged:", name: NSHTTPCookieManagerCookiesChangedNotification, object: nil)
     }
 }
